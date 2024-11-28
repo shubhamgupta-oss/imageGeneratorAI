7aeff31a81d2eb449f04ed0bb81daaa3a0c90ade
Shubham Gupta <shubhamagrahari18@gmail.com>
2024-11-28 12:12:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,29 +4,33 @@ import connectMongodb from './Connection/Mongo.js';
 import loginregister from './Routes/loginregister.js';
 import imagesRelated from './Routes/imagesRoute.js';
 import bodyParser from 'body-parser';
-import cors  from 'cors';
-
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
-app.options('*', cors()) 
-app.use(bodyParser.json());  
 
 const port = process.env.PORT || 3001;
 
+app.use(cors({
+    origin: "https://imagegeneratorai-1.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.use(bodyParser.json());
 app.use(express.json());
 
 app.use("/api", loginregister);
 app.use("/api", imagesRelated);
 
-
 connectMongodb(process.env.MONGO_URL)
   .then(() => console.log("Mongo Connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-// Global error handling middleware (for unhandled routes or errors)
+
 app.use((err, req, res, next) => {
-  console.error(err.stack);  
+  console.error(err.stack);
   res.status(500).json({ message: "Something went wrong!" });
 });
 
