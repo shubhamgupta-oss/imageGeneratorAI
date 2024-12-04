583123c0de7bdd83ec0ec4e978cf9c1251ecda19
shubham <shubhamagrahari18@gmail.com>
2024-12-04 18:45:29 +0530

    pageNotFound

--- client/src/component/404/NotFound.jsx
+++ client/src/component/404/NotFound.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import './NotFound.css';
 
 function NotFound() {
   return (
-    <div>
+    <div className="NotFound">
       <h1>404 - Page Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
+      <button onClick={() => window.location.href = '/'}>Go Back Home</button>
     </div>
   );
 }
